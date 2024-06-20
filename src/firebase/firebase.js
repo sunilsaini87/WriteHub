b76c3e6ba4a616b318d4f396a538d597150457aa
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-06-20 17:37:32 +0530

    Add initial files

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASXB1WPmN23DPdKMQcx-Ucir9v9KpUQZs",
+  authDomain: "writehub-523a3.firebaseapp.com",
+  projectId: "writehub-523a3",
+  storageBucket: "writehub-523a3.appspot.com",
+  messagingSenderId: "331232756670",
+  appId: "1:331232756670:web:20f348cb80f68f03e6efef",
+  measurementId: "G-XC4QC4TRP9",
+};
+
+const app = initializeApp(firebaseConfig);
