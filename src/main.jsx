b3f9a51fa55e0c1fecf12098101b2732007511b6
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-06-21 15:22:03 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Context from "./Context/Context.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import "react-quill/dist/quill.bubble.css";
+import "react-tagsinput/react-tagsinput.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Context>
+        <App />
+      </Context>
     </BrowserRouter>
   </React.StrictMode>
 );
