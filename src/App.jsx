02c8568be003883ea0d8a020f2f92551f3607c0e
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-06-21 12:54:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Demo from "./components/Demo/Demo";
+
 function App() {
   return (
     <>
-      <h1 className="text-cyan-600">Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
     </>
   );
 }
