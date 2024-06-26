1988a35de458dedf75c65c473263d26e511ee951
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-06-26 10:09:41 +0530

    update

--- src/components/Demo/Banner.jsx
+++ src/components/Demo/Banner.jsx
@@ -3,13 +3,14 @@ const Banner = () => {
     <div className="bg-banner border-b border-black">
       <div className="size py-[5rem] flex flex-col items-start gap-[1rem]">
         <h1 className="font-title text-[3rem] sm:text-[4rem] md:text-[6rem] font-normal">
-          Stay curious.
+          Embrace Curiosity
         </h1>
         <p className="w-full md:w-[31rem] text-[1.3rem] md:text-[1.5rem] font-medium leading-7">
-          Discover stories, thinking, and expertise from writers on any topic.
+          Explore insights, thoughts, and knowledge from writers on a myriad of
+          subjects.
         </p>
         <button className="btn bg-black1 rounded-full text-white !text-[1.2rem] !px-6 !mt-[2.5rem]">
-          Start reading
+          Start Exploring
         </button>
       </div>
     </div>
--- src/utils/Modal.jsx
+++ src/utils/Modal.jsx
@@ -1,16 +1,44 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 
 const Modal = ({ children, modal, setModal }) => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        console.log("Escape key pressed");
+        setModal(false);
+      }
+    };
+
+    if (modal) {
+      document.addEventListener("keydown", handleKeyDown);
+    } else {
+      document.removeEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [modal, setModal]);
+
+  const handleBackgroundClick = () => {
+    setModal(false);
+  };
+
   return (
     <>
       <div
-        onClick={() => setModal(false)}
-        className={`bg-white/50 fixed inset-0 z-10 
-        ${
-          modal ? "visible opacity-100" : "invisible opacity-0"
-        } transition-all duration-500`}
+        onClick={handleBackgroundClick}
+        className={`fixed inset-0 z-10 bg-black bg-opacity-50 transition-opacity duration-500
+        ${modal ? "opacity-100 visible" : "opacity-0 invisible"}`}
       />
-      {children}
+      <div
+        className={`fixed inset-0 z-20 flex items-center justify-center pointer-events-none
+        ${modal ? "opacity-100 visible" : "opacity-0 invisible"} 
+        transition-opacity duration-500`}
+      >
+        <div className="pointer-events-auto">{children}</div>
+      </div>
     </>
   );
 };
