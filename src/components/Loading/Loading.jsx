1ae83e7e4ad41d9962f54654870feb230f33b776
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-06-26 13:30:08 +0530

    update

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,11 @@
 const Loading = () => {
   return (
     <div className="fixed inset-0 grid place-items-center bg-white z-30">
-      <img className="w-[20rem]" src="/loading2.gif" alt="loading" />
+      <img
+        className="w-[20rem]"
+        src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZml0MDdqaDM0eGVmZGx1eTNpZ2czbW02MGI1MjQ2cmE2cWFzcDg2diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/y1ZBcOGOOtlpC/200w.webp"
+        alt="loading"
+      />
     </div>
   );
 };
